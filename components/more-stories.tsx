cbf9de07ef44f883a517057851f543b730774709
Matt Dole <mdole7@gmail.com>
2021-08-23 22:01:27 +0200

    chore: remove a lot of unused code

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -1,31 +1,29 @@
-import PostPreview from './post-preview'
-import Post from '../types/post'
+import Link from "next/link";
+import React from "react";
+import Post from "../types/post";
+import DateFormatter from "./date-formatter";
 
 type Props = {
-  posts: Post[]
-}
+  posts: Post[];
+};
 
 const MoreStories = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
-      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
-          />
+          <div>
+            <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
+              <a className="hover:underline">{post.title}</a>
+            </Link>
+            <div className="text-lg mb-4">
+              <DateFormatter dateString={post.date} />
+            </div>
+          </div>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MoreStories
+export default MoreStories;

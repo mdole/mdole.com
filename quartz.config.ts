f7a7f4f2651ea19288389c7d9f15e02897e2ed43
Matt Dole <5361806+mdole@users.noreply.github.com>
2024-01-13 10:37:01 +0100

    purpleize

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,9 +22,9 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
+          dark: "#20124d",
+          secondary: "#381f86",
+          tertiary: "#a28de4",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
